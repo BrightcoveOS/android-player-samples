b428f4087d14e7bbf220246eea038dda9b83f9e4
preilly <preilly@brightcove.com>
2013-12-04 21:23:27 -0500

    Removed copyright.

--- BasicIMASampleApp/src/instrumentTest/java/com/brightcove/player/samples/ima/basic/MainActivityTest.java
+++ BasicIMASampleApp/src/instrumentTest/java/com/brightcove/player/samples/ima/basic/MainActivityTest.java
@@ -1,20 +1,3 @@
-/**
- * Copyright (C) 2012 Brightcove, Inc. All Rights Reserved. No
- * use, copying or distribution of this work may be made except in
- * accordance with a valid license agreement from Brightcove, Inc.
- * This notice must be included on all copies, modifications and
- * derivatives of this work.
- *
- * Brightcove, Inc MAKES NO REPRESENTATIONS OR WARRANTIES ABOUT
- * THE SUITABILITY OF THE SOFTWARE, EITHER EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, OR
- * NON-INFRINGEMENT. BRIGHTCOVE SHALL NOT BE LIABLE FOR ANY DAMAGES SUFFERED
- * BY LICENSEE AS A RESULT OF USING, MODIFYING OR DISTRIBUTING THIS
- * SOFTWARE OR ITS DERIVATIVES.
- *
- * "Brightcove" is a trademark of Brightcove, Inc.
- */
 package com.brightcove.player.samples.ima.basic;
 
 import android.test.ActivityInstrumentationTestCase2;
