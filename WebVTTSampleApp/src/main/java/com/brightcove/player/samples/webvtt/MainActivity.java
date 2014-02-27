574f9f7a042b4bf4d2e82e2e25afcf7088a3cb2f
preilly <preilly@brightcove.com>
2014-02-27 05:19:43 -0500

    Initial checkin of WebVTT sample application.  Cleaned up the lint warnings while I was at it.
    AdRulesIMASampleApp/src/main/AndroidManifest.xml
    BasicAdobePassWebViewSampleApp/src/main/AndroidManifest.xml
    BasicBundledVideoSampleApp/src/main/AndroidManifest.xml
    BasicFreeWheelSampleApp/src/main/AndroidManifest.xml
    BasicIMASampleApp/src/main/AndroidManifest.xml
    BasicIMAWidevineSampleApp/src/main/AndroidManifest.xml
    BasicOmnitureSampleApp/src/main/AndroidManifest.xml
    BasicWidevineSampleApp/src/main/AndroidManifest.xml
    HLSPlayerSampleApp/src/main/AndroidManifest.xml
    
      * Removed the <uses-sdk> element, because the values are being set
        via gradle.
    
    AdRulesIMASampleApp/src/main/res/layout/ima_activity_main.xml
    BasicAISWebViewSampleApp/src/main/res/layout/ais_activity_main.xml
    BasicAISWebViewSampleApp/src/main/res/layout/ais_webview_activity_main.xml
    BasicAdobePassWebViewSampleApp/src/main/res/layout/adobepass_activity_main.xml
    BasicAdobePassWebViewSampleApp/src/main/res/layout/adobepass_webview_activity_main.xml
    BasicBundledVideoSampleApp/src/main/res/layout/bundled_video_activity_main.xml
    BasicFreeWheelSampleApp/src/main/res/layout/freewheel_activity_main.xml
    BasicIMASampleApp/src/main/res/layout/ima_activity_main.xml
    BasicIMAWidevineSampleApp/src/main/res/layout/ima_widevine_activity_main.xml
    BasicOmnitureSampleApp/src/main/res/layout/omniture_activity_main.xml
    BasicWidevineSampleApp/src/main/res/layout/basic_widevine.xml
    HLSPlayerSampleApp/src/main/res/layout/activity_main.xml
    
      * Replaced top level tag with <merge> as per Android best practices.
    
    BasicBundledVideoSampleApp/src/main/java/com/brightcove/samples/android/bundledvideo/basic/MainActivity.java
    
      * Removed unused TAG constant.  This change can be reverted if we add some logging.
    
    BasicFreeWheelSampleApp/build.gradle
    BasicOmnitureSampleApp/build.gradle
    
      * Added "@aar" to end of dependency, so the legacy jar isn't used.
    
    BasicWidevineSampleApp/src/main/res/values-w820dp/dimens.xml
    
      * Not used.
    
    WebVTTSampleApp/build.gradle
    WebVTTSampleApp/src/main/AndroidManifest.xml
    WebVTTSampleApp/src/main/ic_launcher-web.png
    WebVTTSampleApp/src/main/java/com/brightcove/player/samples/webvtt/MainActivity.java
    WebVTTSampleApp/src/main/res/drawable-hdpi/ic_launcher.png
    WebVTTSampleApp/src/main/res/drawable-mdpi/ic_launcher.png
    WebVTTSampleApp/src/main/res/drawable-xhdpi/ic_launcher.png
    WebVTTSampleApp/src/main/res/drawable-xxhdpi/ic_launcher.png
    WebVTTSampleApp/src/main/res/layout/activity_main.xml
    WebVTTSampleApp/src/main/res/raw/sintel_trailer.m4v
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_de.vtt
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_en.vtt
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_es.vtt
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_fr.vtt
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_it.vtt
    WebVTTSampleApp/src/main/res/raw/sintel_trailer_nl.vtt
    WebVTTSampleApp/src/main/res/values-v11/styles.xml
    WebVTTSampleApp/src/main/res/values-v14/styles.xml
    WebVTTSampleApp/src/main/res/values/strings.xml
    WebVTTSampleApp/src/main/res/values/styles.xml
    
      * Initial checkin.
    
    build.gradle
    
      * Added "@aar" to end of android-native-player dependencies to the
        legacy jars are not used.
    
      * Bumped up the targetSdkVersion to 19.
    
      * Added mavenLocal() to the reposititories to add local development
        and changed from docs.brightcove.com repo to
        solutions.brightcove.com repo.
    
    gradle.properties
    
      * Changed anpVersion to 4.+ in anticipation of first 4.1 release.
    
      * Added runWebVTTSampleApp flag.
    
    settings.gradle
    
      * Added runWebVTTSampleApp logic.

---
+++ WebVTTSampleApp/src/main/java/com/brightcove/player/samples/webvtt/MainActivity.java
@@ -0,0 +1,36 @@
+package com.brightcove.player.samples.webvtt;
+
+import android.media.MediaFormat;
+import android.net.Uri;
+import android.os.Bundle;
+import com.brightcove.player.view.BrightcovePlayer;
+import com.brightcove.player.view.BrightcoveVideoView;
+
+/**
+ * This activity demonstrates how to play a video with closed
+ * captions in multiple languages.
+ */
+public class MainActivity extends BrightcovePlayer {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setContentView(R.layout.activity_main);
+        brightcoveVideoView = (BrightcoveVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
+
+        brightcoveVideoView.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.sintel_trailer));
+
+        MediaFormat mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "de");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_de), mediaFormat);
+        mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "en");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_en), mediaFormat);
+        mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "es");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_es), mediaFormat);
+        mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "fr");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_fr), mediaFormat);
+        mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "it");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_it), mediaFormat);
+        mediaFormat = MediaFormat.createSubtitleFormat("text/vtt", "nl");
+        brightcoveVideoView.addSubtitleSource(getResources().openRawResource(R.raw.sintel_trailer_nl), mediaFormat);
+    }
+}
