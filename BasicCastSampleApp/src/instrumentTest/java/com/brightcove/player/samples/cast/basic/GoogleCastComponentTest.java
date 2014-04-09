22c0b2336f3bae2b036e1ab8baa0b14aff60285f
Billy Hnath <bhnath@brightcove.com>
2014-04-09 17:44:39 -0400

    working integration tests

--- BasicCastSampleApp/src/instrumentTest/java/com/brightcove/player/samples/cast/basic/GoogleCastComponentTest.java
+++ BasicCastSampleApp/src/instrumentTest/java/com/brightcove/player/samples/cast/basic/GoogleCastComponentTest.java
@@ -19,15 +19,17 @@ package com.brightcove.player.samples.cast.basic;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
+import android.view.View;
 
-import com.brightcove.cast.GoogleCastComponent;
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
 import com.brightcove.player.event.EventType;
 import com.brightcove.player.view.BrightcoveVideoView;
+import com.google.sample.castcompanionlibrary.cast.player.VideoCastControllerActivity;
 import com.robotium.solo.Solo;
 
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -43,8 +45,6 @@ public class GoogleCastComponentTest extends ActivityInstrumentationTestCase2<Ma
     private MainActivity castActivity;
     private BrightcoveVideoView brightcoveVideoView;
     private EventEmitter eventEmitter;
-    private GoogleCastComponent component;
-    private GoogleCastSampleFragment googleCastSampleFragment;
     private Solo solo;
 
     public GoogleCastComponentTest() {
@@ -54,18 +54,14 @@ public class GoogleCastComponentTest extends ActivityInstrumentationTestCase2<Ma
     @Override
     protected void setUp() throws Exception {
         super.setUp();
+        setActivityInitialTouchMode(true);
         castActivity = getActivity();
-        setActivityInitialTouchMode(false);
 
         solo  = new Solo(getInstrumentation(), castActivity);
         brightcoveVideoView = (BrightcoveVideoView) castActivity.findViewById(R.id.brightcove_video_view);
         eventEmitter = brightcoveVideoView.getEventEmitter();
-        googleCastSampleFragment = castActivity.googleCastSampleFragment;
-        castActivity.launchBrightcoveVideoViewFragment();
 
-        brightcoveVideoView.setVideoPath("https://ia600408.us.archive.org/26/items/BigBuckBunny_328/BigBuckBunny_512kb.mp4");
-
-        final CountDownLatch countDownLatch = new CountDownLatch(1);
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
         eventEmitter.once(EventType.DID_SET_VIDEO, new EventListener() {
             @Override
             public void processEvent(Event event) {
@@ -73,40 +69,101 @@ public class GoogleCastComponentTest extends ActivityInstrumentationTestCase2<Ma
                 countDownLatch.countDown();
             }
         });
+
+        eventEmitter.once(EventType.DID_PLAY, new EventListener() {
+            @Override
+            public void processEvent(Event event) {
+                assertTrue("BrightcoveVideoView is playing.", brightcoveVideoView.isPlaying());
+                countDownLatch.countDown();
+            }
+        });
+
         assertTrue("Timeout occured.", countDownLatch.await(1, TimeUnit.MINUTES));
     }
 
     /**
-     * Test
-     * From a local playback state currently playing
-     * Selecting the cast button will start playback on the remote device
-     * and pause playback on the local device.
-     *
+     * Test the successful local to remote playback and then remote to local
+     * with the media starting in the PLAY state on the local device.
      * @throws InterruptedException
      */
-    public void testCastMediaToRemoteDevice() throws InterruptedException {
-        Log.v(TAG, "testCastMediaToRemoteDevice");
-        final CountDownLatch countDownLatch = new CountDownLatch(1);
-
-        //assertTrue("BrightcoveVideoView is playing.", brightcoveVideoView.isPlaying());
+    public void testCastMediaToRemoteDeviceAndBackFromPlaying() throws InterruptedException {
+        Log.v(TAG, "testCastMediaToRemoteDeviceAndBackFromPlaying");
+        final CountDownLatch countDownLatch = new CountDownLatch(2);
 
         solo.clickOnActionBarItem(R.id.media_router_menu_item);
         solo.clickInList(0);
 
-        eventEmitter.on(EventType.DID_PAUSE, new EventListener() {
+        eventEmitter.once(EventType.DID_PAUSE, new EventListener() {
             @Override
             public void processEvent(Event event) {
                 assertFalse("BrightVideoVideo is paused.", brightcoveVideoView.isPlaying());
                 countDownLatch.countDown();
             }
         });
+        solo.waitForActivity(VideoCastControllerActivity.class);
+        solo.sleep(10000);
 
-        assertTrue("Timeout occurred.", countDownLatch.await(1, TimeUnit.MINUTES));
+        solo.assertCurrentActivity("VideoCastControllerActivity is displayed", VideoCastControllerActivity.class);
+
+        List<View> views = solo.getCurrentViews();
+        for (View v : views) {
+            if (v instanceof android.support.v7.app.MediaRouteButton) {
+                solo.clickOnView(v);
+            }
+        }
+        solo.sleep(3000);
+
+        solo.clickOnButton("Disconnect");
+        solo.sleep(3000);
+
+        countDownLatch.countDown();
+
+        assertTrue("Timeout occurred.", countDownLatch.await(3, TimeUnit.MINUTES));
+    }
+
+    /**
+     * Test the successful local to remote playback and then remote to local
+     * with the media starting in the PAUSE state on the local device.
+     *
+     * @throws InterruptedException
+     */
+    public void testCastMediaToRemoteDeviceAndBackFromPaused() throws InterruptedException {
+        Log.v(TAG, "testCastMediaToRemoteDeviceAndBackFromPaused");
+        final CountDownLatch countDownLatch = new CountDownLatch(1);
+
+        brightcoveVideoView.stopPlayback();
+        solo.sleep(3000);
+
+        solo.clickOnActionBarItem(R.id.media_router_menu_item);
+        solo.clickInList(0);
+        solo.sleep(6000);
+
+        brightcoveVideoView.start();
+
+        solo.waitForActivity(VideoCastControllerActivity.class);
+        solo.sleep(10000);
+
+        solo.assertCurrentActivity("VideoCast", VideoCastControllerActivity.class);
+
+        List<View> views = solo.getCurrentViews();
+        for (View v : views) {
+            if (v instanceof android.support.v7.app.MediaRouteButton) {
+                solo.clickOnView(v);
+            }
+        }
+        solo.sleep(3000);
+
+        solo.clickOnButton("Disconnect");
+        solo.sleep(3000);
+
+        countDownLatch.countDown();
+
+        assertTrue("Timeout occurred.", countDownLatch.await(3, TimeUnit.MINUTES));
     }
 
     @Override
     public void tearDown() throws Exception {
-        super.tearDown();
         //solo.finishOpenedActivities();
+        super.tearDown();
     }
 }
