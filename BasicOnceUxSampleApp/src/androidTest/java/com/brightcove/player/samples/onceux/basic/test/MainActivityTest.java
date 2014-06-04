d628f9807f9a9964ccdfecc9efdb5535d2f60e2d
Bryan Scott <pmreilly@brightcove.com>
2014-06-04 15:23:22 -0400

    Revised NO_AD_DATA_URL test, added WifiManager, and AD_DATA_READY test

--- BasicOnceUxSampleApp/src/androidTest/java/com/brightcove/player/samples/onceux/basic/test/MainActivityTest.java
+++ BasicOnceUxSampleApp/src/androidTest/java/com/brightcove/player/samples/onceux/basic/test/MainActivityTest.java
@@ -1,18 +1,15 @@
 package com.brightcove.player.samples.onceux.basic.test;
 
-import android.content.Intent;
+import android.net.wifi.WifiManager;
 import android.os.CountDownTimer;
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
-import android.view.View;
-import android.provider.Settings;
-import android.widget.Toast;
 import android.content.Context;
 
-import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
 import com.brightcove.player.event.EventType;
+import com.brightcove.player.event.Event;
 import com.brightcove.player.samples.onceux.basic.MainActivity;
 import com.brightcove.player.samples.onceux.basic.R;
 import com.brightcove.player.view.BrightcoveVideoView;
@@ -49,14 +46,53 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
             }
         });
     }
-
     public void testNoAdData() throws InterruptedException {
+        mainActivity.getOnceUxPlugin().processVideo(null, "http://cdn5.unicornmedia.com/now/stitched/mp4/95ea75e1-dd2a-4aea-851a-28f46f8e8195/00000000-0000-0000-0000-000000000000/3a41c6e4-93a3-4108-8995-64ffca7b9106/9b118b95-38df-4b99-bb50-8f53d62f6ef8/0/0/105/1438852996/content.mp4");
         Log.v(TAG, "testNoAdDataURL");
         eventEmitter.on(OnceUxEventType.NO_AD_DATA_URL, new EventListener() {
             @Override
             public void processEvent(Event event) {
-                fail("This should have never happened; there is no Ad Data URL.");
-            }
+                assertTrue("This should have never happened; there is no Ad Data URL.", false);
+            }           
         });
+        eventEmitter.emit(EventType.PLAY);
+    }
+
+    public void testWifiOff() throws InterruptedException {
+        WifiManager wifiManager = (WifiManager) this.getActivity().getSystemService(Context.WIFI_SERVICE);
+        boolean wifiResult = wifiManager.setWifiEnabled(false);
+        Log.v(TAG, "Wifi should be off " + wifiResult);
+        //Turning off Wifi to trigger an Error in the next test for the AD_DATA_READY.
+    }
+
+    public void testTimer() throws InterruptedException {
+        Log.v(TAG, "Timer!");
+        final CountDownTimer timer = new CountDownTimer(1000,1000) {
+                @Override
+                public void onTick(long millisUntilFinished){}
+                @Override
+                public void onFinish() {
+                    fail("Needs Ad Data");
+                }
+            };
+        
+        // We want to have another listener that is listening for the AD_READY, and inside that listener, we're going to do something!!!
+        eventEmitter.on(OnceUxEventType.AD_DATA_READY, new EventListener() {
+                @Override
+                public void processEvent(Event event) {
+                    timer.cancel();
+                    String errorMessage = (String) event.properties.get(OnceUxEventType.VMAP_ERRORS);
+                    String responseMessage = (String) event.properties.get(OnceUxEventType.VMAP_RESPONSE);
+                    if (errorMessage == null && responseMessage == null) {
+                        fail("Error: AD_DATA_READY is empty");
+                    } else if(errorMessage != null && responseMessage != null) {
+                        fail("Error: AD_DATA_READY is too full");
+                    } else if(errorMessage != null) {
+                        fail("Error: AD_DATA_READY has at least one error");
+                    } else {
+                        assertTrue(true);
+                    }
+                };
+            });
     }
 }
