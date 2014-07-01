3e82f84cfe69065857218e2eb6483a68d4e7831e
Bryan Scott <pmreilly@brightcove.com>
2014-07-01 15:22:00 -0400

    Making fixes to LifeCycle.java
    ##Changed Files
    LifeCycle.java
    * included TimeUnit sleep strategy
    * reordering methods to follow the traditional pattern
    * omitting test method testLifeCycleBeforeAdDataReady, as the error that was causing the plugin to crash before adDataReady is no longer a problem.
    * omitting test method testLifeCycleEndAdBreak, as it was envisioned to be testing an issue that will be tested in SeekTests.java
    * added tags for each test method identifying when they begin and end.
    * tweaked playVideo wait time
    * added try-catch blocks for the most common UiObjectNotFoundExceptions as they are very easily remedied.

--- BasicOnceUxSampleApp/src/uiTest/java/com/brightcove/player/samples/onceux/basic/test/LifeCycle.java
+++ BasicOnceUxSampleApp/src/uiTest/java/com/brightcove/player/samples/onceux/basic/test/LifeCycle.java
@@ -1,6 +1,5 @@
 package com.brightcove.player.samples.onceux.basic.test;
 
-import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
 import android.util.Log;
@@ -16,7 +15,7 @@ import com.android.uiautomator.testrunner.UiAutomatorTestCase;
  * of the playhead position if the user hits the home button while playing
  * or paused, or during specific events in the timeline.
  */
-public class LifeCycle extends UiAutomatorBaseTest {
+public class LifeCycle extends OnceUxUiAutomatorBaseTestCase {
 
     /**
      * The Android log tag.
@@ -34,6 +33,75 @@ public class LifeCycle extends UiAutomatorBaseTest {
     private String adTimeStringAfterLifeCycle;
 
 
+    // Test Methods (makes assertions and specifies the location of testing)
+
+    //Tests during Ad Breaks
+    /**
+     * testLifeCycleAdBreakPlaying tests the life cycle of the app under the circumstances
+     * that the user leaves the app by pressing the home button and does not close it in 
+     * any way. It calls upon four of the five utility methods. First, it plays the video.
+     * Then, it performs lifeCycleInitialCheck, exiting the video, and returning. The video
+     * is quickly paused to keep the current time, at which point the time is documented,
+     * and then the comparison occurrs. The test asserts stringComparison will return true.
+     */
+    public void testLifeCycleAdBreakPlaying() throws Exception {
+        Log.v(TAG, "Beginning testLifeCycleAdBreakPlaying");
+        playVideo();
+        TimeUnit.SECONDS.sleep(5);
+        lifeCycleInitialCheck();
+        pauseVideo();
+        lifeCycleFollowUpCheck();
+        assertTrue("Strings not identical.", stringComparison());
+        Log.v(TAG, "Finished testLifeCycleAdBreakPlaying");
+    }
+    /**
+     * testLifeCycleAdBreakPaused performs much the same as testLifeCycleAdBreakPlaying,
+     * with one exception: it waits five seconds after play has begun, then pauses the 
+     * video *before* launching lifeCycleInitialCheck. The rest of the process is identical.
+     */
+    public void testLifeCycleAdBreakPaused() throws Exception {
+        Log.v(TAG, "Beginning testLifeCycleAdBreakPaused");
+        playVideo();
+        TimeUnit.SECONDS.sleep(5);
+        pauseVideo();
+        lifeCycleInitialCheck();
+        lifeCycleFollowUpCheck();
+        assertTrue("Strings not identical.", stringComparison());
+        Log.v(TAG, "Finished testLifeCycleAdBreakPaused");
+    }
+
+    //Tests during content blocks
+    /**
+     * testLifeCycleContentBlockPlaying performs similar to testLifeCycleAdBreakPlaying, but
+     * it waits 40 seconds (for the preroll ad to finish), then begins lifeCycleInitialCheck.
+     * It follows the same pattern as testLifeCycleAdBreakPlaying for the rest of the check.
+     */
+    public void testLifeCycleContentBlockPlaying() throws Exception {
+        Log.v(TAG, "Beginning testLifeCycleContentBlockPlaying");
+        playVideo();
+        TimeUnit.SECONDS.sleep(35);
+        lifeCycleInitialCheck();
+        pauseVideo();
+        lifeCycleFollowUpCheck();
+        assertTrue("Strings not identical.", stringComparison());
+        Log.v(TAG, "Finished testLifeCycleContentBlockPlaying");
+    }
+    /**
+     * testLifeCycleContentBlockPaused performs exactly like testLifeCycleContentBlockPlaying,
+     * but pauses the video before beginning lifeCycleInitialCheck.
+     */
+    public void testLifeCycleContentBlockPaused() throws Exception {
+        Log.v(TAG, "Beginning testLifeCycleContentBlockPaused");
+        playVideo();
+        TimeUnit.SECONDS.sleep(35);
+        pauseVideo();
+        lifeCycleInitialCheck();
+        lifeCycleFollowUpCheck();
+        assertTrue("Strings not identical.", stringComparison());
+        Log.v(TAG, "Finished testLifeCycleContentBlockPaused");
+    }
+
+
     // Utility Methods
 
     /**
@@ -43,16 +111,13 @@ public class LifeCycle extends UiAutomatorBaseTest {
      * and in order to keep the setUp method universal across all test cases, play was kept separate.
      */
     private void playVideo() throws Exception {
-        // First, wait for the Sample App to entirely process the video and we tap the screen to reveal the seek controls.
-        latch.await(10, TimeUnit.SECONDS);
-        Log.v(TAG, "Pressing 500, 500 to reveal seek controls and play button.");
+        // First, wait for the Sample App to entirely process the video and we tap the screen to reveal the seek controls and press play.
+        TimeUnit.SECONDS.sleep(6);
+        Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
         getUiDevice().click(500, 500);
-        // Next, we press the play button, initiating play, and hide the seek controls by tapping the screen.
         UiObject playButton = new UiObject(new UiSelector().resourceId("android:id/pause"));
         Log.v(TAG, "Pressing Play...");
         playButton.click();
-        Log.v(TAG, "Pressing 500, 500 to hide seek controls and play button.");
-        getUiDevice().click(500, 500);
     }
 
     /**
@@ -63,14 +128,12 @@ public class LifeCycle extends UiAutomatorBaseTest {
      * mapped to a single resource id, "android:id/pause".
      */
     private void pauseVideo() throws Exception {
-        // First, we bring up the play/seek control menu, then press pause, then, hide the controls again.
-        Log.v(TAG, "Pressing 500, 500 to reveal seek controls and play button.");
+        // First, we bring up the play/seek control menu, then press pause.
+        Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
         getUiDevice().click(500, 500);
         UiObject pauseButton = new UiObject(new UiSelector().resourceId("android:id/pause"));
         Log.v(TAG, "Pressing Pause...");
         pauseButton.click();
-        Log.v(TAG, "Pressing 500, 500 to hide seek controls and play button.");
-        getUiDevice().click(500, 500);
     }
 
     /**
@@ -82,14 +145,22 @@ public class LifeCycle extends UiAutomatorBaseTest {
     private void lifeCycleInitialCheck() throws Exception {
         Log.v(TAG, "Beginning Life Cycle Check.");
         // First, to make note of the playhead position, we reveal seek controls and examine the text view that has the time elapsed. 
-        Log.v(TAG, "Pressing 500, 500 to hide seek controls and play button.");
+        Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
         getUiDevice().click(500, 500);
         UiObject adTimeBeforeLifeCycle = new UiObject(new UiSelector().textStartsWith("00:").index(0));
-        // Then, we make note of that text in a string, and leave the app.
-        adTimeStringBeforeLifeCycle = adTimeBeforeLifeCycle.getText();
+        // Because of the slight inconsistency of the Sample App, we set up try-catch blocks that will be prepared for an exception.
+        try {
+            adTimeStringBeforeLifeCycle = adTimeBeforeLifeCycle.getText();
+        } catch (UiObjectNotFoundException uiPlayheadPositionMissing) {
+            Log.v(TAG, "Ad Time not found. Trying again.");
+            // This is often as a result of the seek controls (and consequently the playhead location) being hidden, so we will show them and retry.
+            Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
+            getUiDevice().click(500, 500);
+            adTimeStringBeforeLifeCycle = adTimeBeforeLifeCycle.getText();            
+        }
+        // Then we leave the app, beginning the check, and return to see if everything went as it should.
         getUiDevice().pressHome();
         Log.v(TAG, "Pressing the home button.");
-        // Now we want to get back to the app and see if everything went as it should. So, we reopen the app using the "Recent Apps" menu.
         getUiDevice().pressRecentApps();
         Log.v(TAG, "Pressing the recent apps button.");
         UiObject basicOnceUxSampleAppRecentActivity = new UiObject(new UiSelector().description("Basic ONCE UX Sample App"));
@@ -106,10 +177,19 @@ public class LifeCycle extends UiAutomatorBaseTest {
         // Now we document our location now that we have returned to the app and enter the text into a string.
         Log.v(TAG, "Beginning the Follow Up check.");
         getUiDevice().click(500, 500);
-        Log.v(TAG, "Pressing 500, 500 to reveal seek controls and play button.");
+        Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
         UiObject adTimeAfterLifeCycle = new UiObject(new UiSelector().textStartsWith("00:").index(0));
         Log.v(TAG, "Getting text from UiObject");
-        adTimeStringAfterLifeCycle = adTimeAfterLifeCycle.getText();
+        // Because of the slight inconsistency of the Sample App, we set up try-catch blocks that will be prepared for an exception.
+        try {
+            adTimeStringAfterLifeCycle = adTimeAfterLifeCycle.getText();
+        } catch (UiObjectNotFoundException uiPlayheadPositionMissing) {
+            Log.v(TAG, "Ad Time not found. Trying again.");
+            // This is often as a result of the seek controls (and consequently the playhead location) being hidden, so we will show them and retry.
+            Log.v(TAG, "Pressing 500, 500 to show seek controls menu.");
+            getUiDevice().click(500, 500);
+            adTimeStringAfterLifeCycle = adTimeAfterLifeCycle.getText();
+        }
     }
 
     /**
@@ -141,87 +221,4 @@ public class LifeCycle extends UiAutomatorBaseTest {
             return false;
         }
     }
-
-    // Test Methods (makes assertions and specifies the location of testing)
-
-    //Specific problematic locations to test
-    public void testLifeCycleBeforeAdDataReady() throws UiObjectNotFoundException {
-        // The period after the video is set and before the ad has loaded was
-        // identified as a problematic area in earlier testing where pressing
-        // the playback control buttons would cause the entire sample app to 
-        // crash, and for the AdDataReady event to never trigger nor play to
-        // ever initialize.
-    }
-    public void testLifeCycleEndAdBreak() throws UiObjectNotFoundException {
-        // The period during an EndAdBreak event and the moments thereafter
-        // were identified as problematic location due to the buggy nature
-        // of the issue and could cause problems with returning to the video
-        // in the correct location, as opposed to the equivalent time with a 
-        // different total time or current location. Should be initiated by 
-        // and END_AD_BREAK event.
-
-        // As it has been discovered that there is currently no way to get 
-        // hold of any events with the UiAutomator testing framework, perhaps
-        // a similar effect could be done with the waitUntilGone method
-        // on the UiObject "adTimeRemaining"
-    }
-
-    //Tests during Ad Breaks
-    /**
-     * testLifeCycleAdBreakPlaying tests the life cycle of the app under the circumstances
-     * that the user leaves the app by pressing the home button and does not close it in 
-     * any way. It calls upon four of the five utility methods. First, it plays the video.
-     * Then, it performs lifeCycleInitialCheck, exiting the video, and returning. The video
-     * is quickly paused to keep the current time, at which point the time is documented,
-     * and then the comparison occurrs. The test asserts stringComparison will return true.
-     */
-    public void testLifeCycleAdBreakPlaying() throws Exception {
-        playVideo();
-        latch.await(5, TimeUnit.SECONDS);
-        lifeCycleInitialCheck();
-        pauseVideo();
-        lifeCycleFollowUpCheck();
-        assertTrue("Strings not identical.", stringComparison());
-    }
-    /**
-     * testLifeCycleAdBreakPaused performs much the same as testLifeCycleAdBreakPlaying,
-     * with one exception: it waits five seconds after play has begun, then pauses the 
-     * video *before* launching lifeCycleInitialCheck. The rest of the process is identical.
-     */
-    public void testLifeCycleAdBreakPaused() throws Exception {
-        playVideo();
-        latch.await(5, TimeUnit.SECONDS);
-        pauseVideo();
-        lifeCycleInitialCheck();
-        lifeCycleFollowUpCheck();
-        assertTrue("Strings not identical.", stringComparison());
-    }
-
-    //Tests during content blocks
-    /**
-     * testLifeCycleContentBlockPlaying performs similar to testLifeCycleAdBreakPlaying, but
-     * it waits 40 seconds (for the preroll ad to finish), then begins lifeCycleInitialCheck.
-     * It follows the same pattern as testLifeCycleAdBreakPlaying for the rest of the check.
-     */
-    public void testLifeCycleContentBlockPlaying() throws Exception {
-        playVideo();
-        latch.await(40, TimeUnit.SECONDS);
-        lifeCycleInitialCheck();
-        pauseVideo();
-        lifeCycleFollowUpCheck();
-        assertTrue("Strings not identical.", stringComparison());
-    }
-    /**
-     * testLifeCycleContentBlockPaused performs exactly like testLifeCycleContentBlockPlaying,
-     * but pauses the video before beginning lifeCycleInitialCheck.
-     */
-    public void testLifeCycleContentBlockPaused() throws Exception {
-        playVideo();
-        latch.await(40, TimeUnit.SECONDS);
-        pauseVideo();
-        lifeCycleInitialCheck();
-        lifeCycleFollowUpCheck();
-        assertTrue("Strings not identical.", stringComparison());
-    }
-
 }
