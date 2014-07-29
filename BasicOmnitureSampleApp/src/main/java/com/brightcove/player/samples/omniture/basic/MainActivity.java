ebf1924886c26dce3102903deb09f8fd76b7dd6a
Billy Hnath <bhnath@brightcove.com>
2014-07-29 16:10:09 -0400

    add json config file and update main activity

--- BasicOmnitureSampleApp/src/main/java/com/brightcove/player/samples/omniture/basic/MainActivity.java
+++ BasicOmnitureSampleApp/src/main/java/com/brightcove/player/samples/omniture/basic/MainActivity.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.RelativeLayout;
 
+import com.adobe.mobile.Media;
 import com.brightcove.omniture.OmnitureComponent;
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
@@ -22,6 +23,8 @@ import com.brightcove.player.model.Playlist;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BrightcovePlayer;
 import com.brightcove.player.view.BrightcoveVideoView;
+
+import java.lang.Override;
 
 /**
  * This app illustrates how to use the Omniture plugin with the Brightcove Player for Android.
@@ -63,71 +66,11 @@ public class MainActivity extends BrightcovePlayer {
 
     private void setupOmniture() {
 
+        // Initializing the Omniture plugin with the JSON configuration file
+        // and evar rulesets should be enough to get started.
         omnitureComponent = new OmnitureComponent(eventEmitter,
                 this,
                 "Android Sample App",
                 "Android Sample Player");
-//        ADMS_Measurement measurement = omnitureComponent.getMeasurement();
-//        measurement.setDebugLogging(true);
-//        measurement.setCharSet("UTF-8");
-//        ADMS_MediaMeasurement mediaMeasurement = omnitureComponent.getMediaMeasurement();
-//        mediaMeasurement.trackSeconds = 8;
-//        mediaMeasurement.trackMilestones = "25,50,75";
-//        mediaMeasurement.segmentByMilestones = true;
-//        mediaMeasurement.trackVars = "eVar1,eVar2,eVar3,prop1,prop2,prop3,prop4";
-//
-//        // eVar2 (account-specific) - Names for the segments
-//        measurement.setEvar(2, "1:beginning,2:second,3:third,4:end");
-//
-//        // eVar3 (account-specific) -Tracking type
-//        measurement.setEvar(3, "video");
-//
-//        // Added to attempt to force a page view.
-//        measurement.track();
-//
-//        eventEmitter.on(EventType.SET_VIDEO, new EventListener() {
-//            @Override
-//            public void processEvent(Event event) {
-//                ADMS_Measurement measurement = omnitureComponent.getMeasurement();
-//                // eVar1 (account-specific) - Video name
-//                Video video = (Video)event.properties.get("video");
-//                String videoName = video.getStringProperty(Video.Fields.NAME);
-//                measurement.setEvar(1, videoName);
-//            }
-//        });
     }
 }
-//
-//omnitureComponent = new OmnitureComponent(eventEmitter,
-//        this,
-//        "ovppbrightcove",
-//        "ovppartners.d1.sc.omtrdc.net", // "scdebugger.com",
-//        "Android Sample App");
-//        ADMS_Measurement measurement = omnitureComponent.getMeasurement();
-//        measurement.setDebugLogging(true);
-//        measurement.setCharSet("UTF-8");
-//        ADMS_MediaMeasurement mediaMeasurement = omnitureComponent.getMediaMeasurement();
-//        mediaMeasurement.trackSeconds = 8;
-//        mediaMeasurement.trackMilestones = "25,50,75";
-//        mediaMeasurement.segmentByMilestones = true;
-//        mediaMeasurement.trackVars = "eVar1,eVar2,eVar3,prop1,prop2,prop3,prop4";
-//
-//        // eVar2 (account-specific) - Names for the segments
-//        measurement.setEvar(2, "1:beginning,2:second,3:third,4:end");
-//
-//        // eVar3 (account-specific) -Tracking type
-//        measurement.setEvar(3, "video");
-//
-//        // Added to attempt to force a page view.
-//        measurement.track();
-//
-//        eventEmitter.on(EventType.SET_VIDEO, new EventListener() {
-//@Override
-//public void processEvent(Event event) {
-//        ADMS_Measurement measurement = omnitureComponent.getMeasurement();
-//        // eVar1 (account-specific) - Video name
-//        Video video = (Video)event.properties.get("video");
-//        String videoName = video.getStringProperty(Video.Fields.NAME);
-//        measurement.setEvar(1, videoName);
-//        }
-//        });
