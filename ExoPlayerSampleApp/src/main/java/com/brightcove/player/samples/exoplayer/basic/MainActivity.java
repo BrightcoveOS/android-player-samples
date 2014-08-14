c045583fc31fedac4a891e2bd85175cab5edc42c
Billy Hnath <bhnath@brightcove.com>
2014-08-14 17:52:27 -0400

    updated with additional dash link

--- ExoPlayerSampleApp/src/main/java/com/brightcove/player/samples/exoplayer/basic/MainActivity.java
+++ ExoPlayerSampleApp/src/main/java/com/brightcove/player/samples/exoplayer/basic/MainActivity.java
@@ -3,6 +3,7 @@ package com.brightcove.player.samples.exoplayer.basic;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BrightcovePlayer;
 import com.brightcove.player.view.ExoPlayerVideoView;
 
@@ -37,11 +38,13 @@ public class MainActivity extends BrightcovePlayer {
 //                Log.e(TAG, error);
 //            }
 //        });
-        //brightcoveVideoView.setVideoPath("https://ia600408.us.archive.org/26/items/BigBuckBunny_328/BigBuckBunny_512kb.mp4");
-        brightcoveVideoView.setVideoPath("http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?"
+//        brightcoveVideoView.setVideoPath("https://ia600408.us.archive.org/26/items/BigBuckBunny_328/BigBuckBunny_512kb.mp4");
+        Video video = Video.createVideo("http://www.youtube.com/api/manifest/dash/id/bf5bb2419360daf1/source/youtube?"
                 + "as=fmp4_audio_clear,fmp4_sd_hd_clear&sparams=ip,ipbits,expire,as&ip=0.0.0.0&"
                 + "ipbits=0&expire=19000000000&signature=255F6B3C07C753C88708C07EA31B7A1A10703C8D."
                 + "2D6A28B21F921D0B245CDCF36F7EB54A2B5ABFC2&key=ik0");
+        video.getProperties().put(Video.Fields.CONTENT_ID, "bf5bb2419360daf1");
+        brightcoveVideoView.add(video);
 
         // Log whether or not instance state in non-null.
         if (savedInstanceState != null) {
