a20f731a0e920b88361a60247721ba9782bc19e5
bhnath <bill.hnath@gmail.com>
2014-11-21 09:24:01 -0500

    add captioning view test case

--- DFXPClosedCaptioningSampleApp/src/androidTest/java/com/brightcove/samples/android/closedcaptioning/dfxp/MainActivityTest.java
+++ DFXPClosedCaptioningSampleApp/src/androidTest/java/com/brightcove/samples/android/closedcaptioning/dfxp/MainActivityTest.java
@@ -2,14 +2,17 @@ package com.brightcove.samples.android.closedcaptioning.dfxp;
 
 import android.test.ActivityInstrumentationTestCase2;
 import android.util.Log;
+import android.view.View;
 
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
 import com.brightcove.player.event.EventType;
+import com.brightcove.player.view.BrightcoveClosedCaptioningView;
 import com.brightcove.player.view.BrightcoveVideoView;
 import com.robotium.solo.Solo;
 
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 
@@ -47,19 +50,23 @@ public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActiv
     }
 
     /**
-     * Test
+     * Verify that upon receiving DFXP captions that a single BrightcoveClosedCaptioningView is
+     * added to the BrightcoveVideoView.
      */
-    public void testCaptionsLoaded() throws InterruptedException {
+    public void testCaptionViewAdded() throws InterruptedException {
         final CountDownLatch latch = new CountDownLatch(1);
-        Log.v(TAG, "testCaptionsLoaded");
+        Log.v(TAG, "testCaptionViewAdded:");
+
         eventEmitter.once(EventType.DID_LOAD_CAPTIONS, new EventListener() {
             @Override
             public void processEvent(Event event) {
-                Log.v(TAG, "DERP CAPS LOADED");
+                View parent = solo.getView(R.id.brightcove_video_view);
+                ArrayList<BrightcoveClosedCaptioningView> captioningViews = solo.getCurrentViews(BrightcoveClosedCaptioningView.class, parent);
+                assertTrue("A BrightcoveClosedCaptioningView was added.", captioningViews.size() == 1);
                 latch.countDown();
             }
         });
-        assertTrue("Timeout occurred.", latch.await(30, TimeUnit.SECONDS));
+        assertTrue("Timeout occurred.", latch.await(2, TimeUnit.MINUTES));
     }
 
     @Override
