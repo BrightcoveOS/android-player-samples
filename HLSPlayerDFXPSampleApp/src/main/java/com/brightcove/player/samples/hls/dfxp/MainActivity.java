b9679d0f87d54de8a56d542cf820bbba2bd9b0a6
BCJwhisenant <jwhisenant@brightcove.com>
2015-02-17 19:14:51 -0500

    Adding a functional test app for HLS+DFXP captions

---
+++ HLSPlayerDFXPSampleApp/src/main/java/com/brightcove/player/samples/hls/dfxp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.brightcove.player.samples.hls.dfxp;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.brightcove.player.media.Catalog;
+import com.brightcove.player.media.VideoListener;
+import com.brightcove.player.model.Video;
+import com.brightcove.player.view.BrightcovePlayer;
+import com.brightcove.player.view.SeamlessVideoView;
+
+/**
+ * This app illustrates how to use the Brightcove HLS player for
+ * Android, with DFXP Captions.
+ *
+ * @author Jim Whisenant
+ */
+public class MainActivity extends BrightcovePlayer {
+
+    private final String TAG = this.getClass().getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before
+        // entering the superclass. This allows for some stock video player lifecycle
+        // management.  Establish the video object and use it's event emitter to get important
+        // notifications and to control logging.
+        setContentView(R.layout.activity_main);
+        brightcoveVideoView = (SeamlessVideoView) findViewById(R.id.brightcove_video_view);
+        super.onCreate(savedInstanceState);
+
+        Catalog catalog = new Catalog("UV3EUeje-jlI5sUpJAGsDZ2jki26BZl78pRKemVDxNTXAxyVOabPdA..");
+        catalog.findVideoByID("3963535001001", new VideoListener() {
+            public void onVideo(Video video) {
+                brightcoveVideoView.add(video);
+            }
+
+            public void onError(String error) {
+                Log.e(TAG, error);
+            }
+        });
+
+        // Log whether or not instance state in non-null.
+        if (savedInstanceState != null) {
+            Log.v(TAG, "Restoring saved position");
+        } else {
+            Log.v(TAG, "No saved state");
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_cc_settings:
+                showClosedCaptioningDialog();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
