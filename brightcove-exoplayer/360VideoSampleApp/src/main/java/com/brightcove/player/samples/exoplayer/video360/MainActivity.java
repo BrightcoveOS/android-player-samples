e192589a01d319cddd37a3edf5b391c486b8d700
Sergio Martinez <smartinez@brightcove.com>
2017-01-26 17:19:12 -0500

    Retrieving video 360 from Brightcove Video Cloud

--- brightcove-exoplayer/360VideoSampleApp/src/main/java/com/brightcove/player/samples/exoplayer/video360/MainActivity.java
+++ brightcove-exoplayer/360VideoSampleApp/src/main/java/com/brightcove/player/samples/exoplayer/video360/MainActivity.java
@@ -1,22 +1,17 @@
 package com.brightcove.player.samples.exoplayer.video360;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
+import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.media.DeliveryType;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
 import com.brightcove.player.view.BrightcovePlayer;
 
 public class MainActivity extends BrightcovePlayer {
-    // Settings for HLS test video
-    private static final String VIDEO_URL = "https://secure.brightcove.com/services/mobile/streaming/index/master.m3u8?videoId=5123538633001&pubId=5028486670001&secure=true";
-    private static final DeliveryType VIDEO_TYPE = DeliveryType.HLS;
-
-    // Settings for MP4 test video
-    //private static final String VIDEO_URL = "https://brightcove.hs.llnwd.net/e1/uds/pd/5028486670001/5028486670001_5123574206001_5123538633001.mp4?pubId=5028486670001&videoId=5123538633001";
-    //private static final DeliveryType VIDEO_TYPE = DeliveryType.MP4;
-
-    private static final Video.ProjectionFormat PROJECTION_FORMAT = Video.ProjectionFormat.EQUIRECTANGULAR;
 
     @Override
     @SuppressWarnings("ResourceType")
@@ -29,24 +24,22 @@ public class MainActivity extends BrightcovePlayer {
         brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
         super.onCreate(savedInstanceState);
 
-        Video video = Video.createVideo(VIDEO_URL, VIDEO_TYPE, PROJECTION_FORMAT);
-        brightcoveVideoView.add(video);
+        EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
+        Catalog catalog = new Catalog(eventEmitter, getString(R.string.account), getString(R.string.policy));
+
+        catalog.findVideoByID(getString(R.string.videoId), new VideoListener() {
+            @Override
+            public void onVideo(Video video) {
+                Video.ProjectionFormat projectionFormat = video.getProjectionFormat();
+                if (projectionFormat == Video.ProjectionFormat.EQUIRECTANGULAR) {
+                    Log.i(TAG, "This is a 360 video");
+                }
+                brightcoveVideoView.add(video);
+                brightcoveVideoView.start();
+            }
+        });
+        //You can also create a 360 video by setting the the projection field on creation as shown below:
+        //Video video = Video.createVideo(VIDEO_URL, VIDEO_TYPE, PROJECTION_FORMAT);
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (isFinishing()) {
-            brightcoveVideoView.stopPlayback();
-        } else {
-            brightcoveVideoView.pause();
-        }
-    }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        brightcoveVideoView.start();
-    }
 }
