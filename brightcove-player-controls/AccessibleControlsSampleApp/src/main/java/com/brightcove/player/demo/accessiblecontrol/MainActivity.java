ba4caa53f0ca8811906f6698473bb8a9f5b3dfbe
mister-ben <git@misterben.me>
2018-10-11 16:57:57 +0100

    simplify example

--- brightcove-player-controls/AccessibleControlsSampleApp/src/main/java/com/brightcove/player/demo/accessiblecontrol/MainActivity.java
+++ brightcove-player-controls/AccessibleControlsSampleApp/src/main/java/com/brightcove/player/demo/accessiblecontrol/MainActivity.java
@@ -2,31 +2,22 @@ package com.brightcove.player.demo.accessiblecontrol;
 
 import com.brightcove.player.edge.Catalog;
 import com.brightcove.player.edge.VideoListener;
-import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
-import com.brightcove.player.event.EventListener;
-import com.brightcove.player.event.EventType;
 import com.brightcove.player.mediacontroller.BrightcoveMediaController;
 import com.brightcove.player.model.Video;
 import com.brightcove.player.view.BaseVideoView;
 import com.brightcove.player.view.BrightcovePlayer;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
 
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 import android.view.accessibility.AccessibilityManager;
 
 /**
  * This app illustrates how to customize the Android default media controller.
  *
- * @author Sergio Martinez
+ * @author Ben Clifford
  */
 public class MainActivity extends BrightcovePlayer {
-    //This TTF font is included in the Brightcove SDK.
-    public static final String FONT_AWESOME = "fontawesome-webfont.ttf";
 
     @Override protected void onCreate(Bundle savedInstanceState) {
         // When extending the BrightcovePlayer, we must assign the BrightcoveVideoView before
@@ -67,21 +58,6 @@ public class MainActivity extends BrightcovePlayer {
         } else {
             brightcoveVideoView.setMediaController(new BrightcoveMediaController(brightcoveVideoView, R.layout.my_media_controller));
         }
-
-
-
-
-        // This event is sent by the BrightcovePlayer Activity when the onConfigurationChanged has been called.
-        brightcoveVideoView.getEventEmitter().on(EventType.CONFIGURATION_CHANGED, new EventListener() {
-            @Override
-            public void processEvent(Event event) {
-                initButtons(brightcoveVideoView);
-            }
-        });
-    }
-
-    private void initButtons(final BaseVideoView brightcoveVideoView) {
-        // Custom buttons could be set up here
     }
 
 }
