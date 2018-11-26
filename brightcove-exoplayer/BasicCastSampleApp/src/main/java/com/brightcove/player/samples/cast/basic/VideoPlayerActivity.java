9427bf0fbb3ba8a48254ee8d77adf73ddff67797
Sergio Martinez <smartinez@brightcove.com>
2018-11-26 08:48:45 -0700

    Added Video ID to intent rather than the Video itself  (#189)
    * Added Video ID to intent rather than the Video itself to avoid Parcel exception with captions.

--- brightcove-exoplayer/BasicCastSampleApp/src/main/java/com/brightcove/player/samples/cast/basic/VideoPlayerActivity.java
+++ brightcove-exoplayer/BasicCastSampleApp/src/main/java/com/brightcove/player/samples/cast/basic/VideoPlayerActivity.java
@@ -12,6 +12,8 @@ import android.widget.TextView;
 import com.brightcove.cast.GoogleCastComponent;
 import com.brightcove.cast.GoogleCastEventType;
 import com.brightcove.player.appcompat.BrightcovePlayerActivity;
+import com.brightcove.player.edge.Catalog;
+import com.brightcove.player.edge.VideoListener;
 import com.brightcove.player.event.Event;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.event.EventListener;
@@ -21,7 +23,7 @@ import com.brightcove.player.view.BrightcoveVideoView;
 
 public class VideoPlayerActivity extends BrightcovePlayerActivity {
 
-    public static final String INTENT_EXTRA_VIDEO = "com.brightcove.player.samples.cast.basic.VideoPlayerActivity.VIDEO";
+    public static final String INTENT_EXTRA_VIDEO_ID = "com.brightcove.player.samples.cast.basic.VideoPlayerActivity.VIDEO_ID";
 
     public static final String PROPS_LONG_DESCRIPTION = "long_description";
     public static final String PROPS_SHORT_DESCRIPTION = "description";
@@ -32,21 +34,30 @@ public class VideoPlayerActivity extends BrightcovePlayerActivity {
         setContentView(R.layout.activity_player);
 
         // Perform the internal wiring to be able to make use of the BrightcovePlayerFragment.
-        Video video  = getIntent().getParcelableExtra(VideoPlayerActivity.INTENT_EXTRA_VIDEO);
         baseVideoView = (BrightcoveVideoView) findViewById(R.id.brightcove_video_view);
         ViewCompat.setTransitionName(baseVideoView, getString(R.string.transition_image));
 
-        String title = video.getName();
-        if (!TextUtils.isEmpty(title)) {
-            TextView textView = findViewById(R.id.video_title_text);
-            textView.setText(title);
-        }
+        String videoId = getIntent().getStringExtra(VideoPlayerActivity.INTENT_EXTRA_VIDEO_ID);
+        Catalog catalog = new Catalog(baseVideoView.getEventEmitter(), getString(R.string.account), getString(R.string.policy));
+        catalog.findVideoByID(videoId, new VideoListener() {
+            @Override
+            public void onVideo(Video video) {
 
-        Object descriptionObj = video.getProperties().get(PROPS_LONG_DESCRIPTION);
-        if (descriptionObj instanceof String) {
-            TextView longDesc = findViewById(R.id.video_description_text);
-            longDesc.setText((String) descriptionObj);
-        }
+                String title = video.getName();
+                if (!TextUtils.isEmpty(title)) {
+                    TextView textView = findViewById(R.id.video_title_text);
+                    textView.setText(title);
+                }
+
+                Object descriptionObj = video.getProperties().get(PROPS_LONG_DESCRIPTION);
+                if (descriptionObj instanceof String) {
+                    TextView longDesc = findViewById(R.id.video_description_text);
+                    longDesc.setText((String) descriptionObj);
+                }
+
+                baseVideoView.add(video);
+            }
+        });
 
         EventEmitter eventEmitter = baseVideoView.getEventEmitter();
 
@@ -69,7 +80,6 @@ public class VideoPlayerActivity extends BrightcovePlayerActivity {
 
         //You can check if there is a session available
         googleCastComponent.isSessionAvailable();
-        baseVideoView.add(video);
     }
 
     @Override
@@ -78,6 +88,7 @@ public class VideoPlayerActivity extends BrightcovePlayerActivity {
         GoogleCastComponent.setUpMediaRouteButton(this, menu);
         return true;
     }
+
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
