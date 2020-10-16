ba75e27f6e922ceb649360ff5626d79c759d88ca
BCJwhisenant <jwhisenant@brightcove.com>
2020-10-16 10:17:44 -0400

    Updated the sample and code doc - Delivery Rule no longer needed

--- brightcove-exoplayer/ThumbnailScrubberSampleApp/src/main/java/com/brightcove/player/samples/thumbnail/MainActivity.java
+++ brightcove-exoplayer/ThumbnailScrubberSampleApp/src/main/java/com/brightcove/player/samples/thumbnail/MainActivity.java
@@ -8,7 +8,6 @@ import com.brightcove.player.edge.VideoListener;
 import com.brightcove.player.event.EventEmitter;
 import com.brightcove.player.mediacontroller.ThumbnailComponent;
 import com.brightcove.player.model.Video;
-import com.brightcove.player.network.HttpRequestConfig;
 import com.brightcove.player.view.BaseVideoView;
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView;
 import com.brightcove.player.view.BrightcovePlayer;
@@ -19,42 +18,35 @@ import com.brightcove.player.view.BrightcovePlayer;
 public class MainActivity extends BrightcovePlayer {
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    @SuppressWarnings("FieldCanBeLocal")
-    private static final String CONFIG_ID_QUERY_PARAM_KEY = "config_id";
-    @SuppressWarnings("FieldCanBeLocal")
-    private static final String CONFIG_ID_THUMBNAIL_SCRUBBING = "8ca8d5b0-5cbd-477b-a4ae-0cf0e85942e7";
+
+    // Note that Edge API v2 is required for the Thumbnail Scrubbing feature.
     @SuppressWarnings("FieldCanBeLocal")
     private static final String EDGE_API_BASE_URL_OVERRIDE = "https://playback.brightcovecdn.com/playback/v1";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before
-        // entering the superclass. This allows for some stock video player lifecycle
-        // management.  Establish the video object and use it's event emitter to get important
-        // notifications and to control logging.
+        // When extending the BrightcovePlayer, we must assign the brightcoveVideoView before entering the superclass.
+        // This allows for some stock video player lifecycle management.
+        // Establish the VideoView object and use its event emitter to get important notifications and to control logging.
         setContentView(R.layout.activity_main);
         brightcoveVideoView = (BrightcoveExoPlayerVideoView) findViewById(R.id.brightcove_video_view);
         configureThumbnailScrubber(brightcoveVideoView);
 
         super.onCreate(savedInstanceState);
 
-        // Get the event emitter from the SDK and create a catalog request to fetch a video from the
-        // Brightcove Edge service, given a video id, an account id and a policy key.
+        // Get the event emitter from the SDK
         EventEmitter eventEmitter = brightcoveVideoView.getEventEmitter();
 
+        // Create a Catalog object to fetch a video from the Brightcove Edge service.
+        // Apply the Edge API override here to point the Catalog to Edge API v2
         Catalog catalog = new Catalog.Builder(eventEmitter, getString(R.string.account))
                             .setBaseURL(EDGE_API_BASE_URL_OVERRIDE)
                             .setPolicy(getString(R.string.policy))
                             .build();
 
-        // Set the HttpRequestConfig with the Ad Config Id configured in
-        // your https://studio.brightcove.com account.
-        HttpRequestConfig httpRequestConfig = new HttpRequestConfig.Builder()
-                .addQueryParameter(CONFIG_ID_QUERY_PARAM_KEY, CONFIG_ID_THUMBNAIL_SCRUBBING)
-                .build();
-
-        catalog.findVideoByID(getString(R.string.videoId), httpRequestConfig, new VideoListener() {
+        // Retrieve the video, given a video id, an account id and a policy key
+        catalog.findVideoByID(getString(R.string.videoId), new VideoListener() {
 
             // Add the video found to the queue with add().
             // Start playback of the video with start().
@@ -67,6 +59,10 @@ public class MainActivity extends BrightcovePlayer {
         });
     }
 
+    /**
+     * Configure the Thumbnail Scrubber using the ThumbnailComponent class
+     * @param brightcoveVideoView       The VideoView object
+     */
     public void configureThumbnailScrubber(BaseVideoView brightcoveVideoView) {
         Log.v(TAG, "Thumbnail Scrubbing is enabled, setting up the PreviewThumbnailController");
         ThumbnailComponent thumbnailComponent = new ThumbnailComponent(brightcoveVideoView);
