1800df049429e06217f0f71a3f0efd13f575f570
BCJwhisenant <jwhisenant@brightcove.com>
2023-03-15 18:04:45 -0400

    Removed a commented line

--- brightcove-exoplayer/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/recyclervideoview/AdapterView.java
+++ brightcove-exoplayer/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/recyclervideoview/AdapterView.java
@@ -31,7 +31,6 @@ public class AdapterView extends RecyclerView.Adapter<AdapterView.ViewHolder> {
 
     private final List<Video> videoList = new ArrayList<>();
     private String adRulesURL = "https://pubads.g.doubleclick.net/gampad/ads?iu=/21775744923/external/single_ad_samples&sz=640x480&cust_params=sample_ct%3Dlinear&ciu_szs=300x250%2C728x90&gdfp_req=1&output=vast&unviewed_position_start=1&env=vp&impl=s&correlator=";
-//    private GoogleIMAComponent googleIMAComponent;
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
