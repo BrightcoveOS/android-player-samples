52e2e5fea2d2ee2847d44268ae6025754a4aaa1f
gcastanheiro <gcastanheiro@brightcove.com>
2025-01-30 14:33:42 +0000

    kotlin based VideoListAdRulesIMASampleApp - fix package name

--- brightcove-exoplayer-kotlin/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/videolist/adrulesima/AdapterView.kt
+++ brightcove-exoplayer-kotlin/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/videolist/adrulesima/AdapterView.kt
@@ -1,4 +1,4 @@
-package com.example.videolist.adrulesima
+package com.brightcove.videolist.adrulesima
 
 import android.content.Context
 import android.util.Log
--- brightcove-exoplayer-kotlin/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/videolist/adrulesima/MainActivity.kt
+++ brightcove-exoplayer-kotlin/VideoListAdRulesIMASampleApp/src/main/java/com/brightcove/videolist/adrulesima/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.videolist.adrulesima
+package com.brightcove.videolist.adrulesima
 
 import android.os.Bundle
 import android.view.View
