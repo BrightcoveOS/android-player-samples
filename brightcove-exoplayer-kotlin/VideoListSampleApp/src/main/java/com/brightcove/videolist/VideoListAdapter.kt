4ca65eff6539492ad5a3b307cb13785e531c42c9
gcastanheiro <gcastanheiro@brightcove.com>
2025-10-22 15:45:55 +0100

    Adapt VideoListSampleApp (kotlin) to better manage BrightcoveExoPlayerVideoView instances in order to avoid memory problems

--- brightcove-exoplayer-kotlin/VideoListSampleApp/src/main/java/com/brightcove/videolist/VideoListAdapter.kt
+++ brightcove-exoplayer-kotlin/VideoListSampleApp/src/main/java/com/brightcove/videolist/VideoListAdapter.kt
@@ -1,5 +1,6 @@
 package com.brightcove.videolist
 
+import android.util.SparseArray
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
@@ -8,10 +9,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.brightcove.player.event.EventType
 import com.brightcove.player.model.Video
 import com.brightcove.player.view.BrightcoveExoPlayerVideoView
-import com.brightcove.player.view.BrightcoveVideoView
 import com.brightcove.videolist.databinding.ItemViewBinding
 
 class VideoListAdapter : ListAdapter<Video, VideoListAdapter.ViewHolder>(DIFF_CALLBACK) {
+
+    // This is used to keep track of the playback position for each video; remove it and its associated logic to play from the start always
+    private val playheadPositions = SparseArray<Long>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemViewBinding.inflate(LayoutInflater.from(parent.context), parent, false)
@@ -20,17 +23,26 @@ class VideoListAdapter : ListAdapter<Video, VideoListAdapter.ViewHolder>(DIFF_CA
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         //Get Video information
-        holder.bind(getItem(position))
+        val video = getItem(position)
+        holder.video = video
+        holder.binding.videoTitleText.text = video.getStringProperty(Video.Fields.NAME)
     }
 
     override fun onViewAttachedToWindow(holder: ViewHolder) {
         super.onViewAttachedToWindow(holder)
-        holder.videoView.start()
+        holder.video?.let { video ->
+            holder.videoView.add(video)
+            holder.videoView.seekTo(playheadPositions.get(holder.absoluteAdapterPosition, 0L))
+            holder.videoView.start()
+        }
     }
 
     override fun onViewDetachedFromWindow(holder: ViewHolder) {
         super.onViewDetachedFromWindow(holder)
+        playheadPositions.put(holder.absoluteAdapterPosition, holder.videoView.currentPositionLong)
         holder.videoView.stopPlayback()
+        holder.videoView.clear()
+        holder.videoView.playback.destroyPlayer()
     }
 
     override fun onDetachedFromRecyclerView(recyclerView: RecyclerView) {
@@ -43,29 +55,20 @@ class VideoListAdapter : ListAdapter<Video, VideoListAdapter.ViewHolder>(DIFF_CA
         }
     }
 
-    class ViewHolder (private val binding: ItemViewBinding
+    class ViewHolder (val binding: ItemViewBinding
     ) : RecyclerView.ViewHolder(binding.root) {
 
-        lateinit var videoView: BrightcoveVideoView
+        val videoView = BrightcoveExoPlayerVideoView(itemView.context)
+        var video: Video? = null
 
-        fun bind(video: Video){
+        init {
+            binding.videoFrame.addView(videoView)
+            videoView.finishInitialization()
 
-            binding.apply {
-
-                videoView = BrightcoveExoPlayerVideoView(itemView.context)
-                videoFrame.addView(videoView)
-                videoView.finishInitialization()
-
-                val eventEmitter = videoView.eventEmitter
-                eventEmitter.on(EventType.ENTER_FULL_SCREEN) {
-                    //You can set listeners on each Video View
-                }
-
-                videoTitleText.text = video.getStringProperty(Video.Fields.NAME)
-                videoView.clear()
-                videoView.add(video)
+            val eventEmitter = videoView.eventEmitter
+            eventEmitter.on(EventType.ENTER_FULL_SCREEN) {
+                //You can set listeners on each Video View
             }
-
         }
     }
 
